eac0f38a32d289b4230ee667342e00e4db794fa2
Igor Nebeschenko <nebeschenkoichor@gmail.com>
2015-11-30 10:00:22 +0200

    select all *.js files

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,7 +1,7 @@
 module.exports = {
   basic_and_extras: {
     files: {
-      'build/js/extended.js':['js/extended.js','js/prototype_to_jquery_ajax_helper.js','js/stub_for_growler.js'],
+      'build/js/extended.js':['js/*.js'],
       'build/js/prototype.js':['lib/jquery/jquery.min.js'],
       'build/js/scriptaculous.js':['lib/materialize/materialize.min.js'],
       'build/css/style.css':['lib/font-awesome/font-awesome.css', 'lib/font-awesome/materialize.min.css'],
